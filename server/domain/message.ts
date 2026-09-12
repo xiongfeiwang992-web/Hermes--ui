26f35f386b9375008fbab9e3dc173cf9143b6999
User <user@example.com>
2026-09-12 15:03:13 +0800

    Integrate entrustment-renew-notify-5bdb
    Source-Commit: 62cedf901a362a88167d856a7713164cd2f92c7f

--- server/domain/message.ts
+++ server/domain/message.ts
@@ -46,6 +46,7 @@ export const MESSAGE_CHANNELS: MessageChannel[] = [
       "verification_pending",
       "verification_review",
       "entrustment_terminated",
+      "entrustment_renewed",
       "transfer_node",
       "mortgage_status",
     ],
